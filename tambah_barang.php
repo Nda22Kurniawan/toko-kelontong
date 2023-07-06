bf8197501596e0bc0a8cf5f305326086f3b3d180
Nanda Dwi Kurniawan <111355409+Nda22Kurniawan@users.noreply.github.com>
2023-07-06 15:49:21 +0700

    Update tambah_barang.php

--- tambah_barang.php
+++ tambah_barang.php
@@ -4,7 +4,7 @@ include("koneksi.php");
 <!doctype html>
 <html lang="en">
   <head>
-  	<title>::Tambah Sata::</title>
+  	<title>::Tambah Data::</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
